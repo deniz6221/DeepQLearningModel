8fa4190bfee2bc843f84170bd78182abd7e060ba
Deniz <deniz.poyraz@std.bogazici.edu.tr>
2025-02-28 23:41:13 +0300

    Fixed some bugs

--- DQN.py
+++ DQN.py
@@ -21,7 +21,7 @@ class DQN(nn.Module):
             nn.Conv2d(256, 512, 4, 2, 1),
             nn.ReLU(),
             nn.AvgPool2d(4),
-            nn.Flatten(0)
+            nn.Flatten(1)
         )
         self.lnetwork = nn.Linear(512, N_actions)
 
@@ -36,22 +36,22 @@ def get_action(epsilon, main_network : DQN, state):
     if rnd < epsilon:
         return random.randint(0,7)
     else:
-        q_vals = main_network(state)
-        act = torch.argmax(q_vals)[0]
+        q_vals = main_network(state.unsqueeze(0))[0]
+        act = torch.argmax(q_vals).item()
         return act
         
 
-def update_params(buffer, batch_size, main_network: DQN, target_network: DQN, optimizer):
+def update_params(buffer, batch_size, main_network: DQN, target_network: DQN, optimizer, gamma):
     if len(buffer) < batch_size:
         return
     
-    batch = random.sample(batch_size)
+    batch = random.sample(buffer, batch_size)
 
-    state_tensor = torch.zeros([batch_size, 3, 128, 128], dtype=torch.double)
-    next_state_tensor = torch.zeros([batch_size, 3, 128, 128], dtype=torch.double)
-    action_tensor = torch.zeros(batch_size, dtype=torch.int)
-    reward_tensor = torch.zeros(batch_size, dtype=torch.double)
-    done_tensor = torch.zeros(batch_size, dtype=torch.int)
+    state_tensor = torch.zeros([batch_size, 3, 128, 128], dtype=torch.float32)
+    next_state_tensor = torch.zeros([batch_size, 3, 128, 128], dtype=torch.float32)
+    action_tensor = torch.zeros(batch_size, dtype=torch.int64)
+    reward_tensor = torch.zeros(batch_size, dtype=torch.float32)
+    done_tensor = torch.zeros(batch_size, dtype=torch.int64)
 
     ctr = 0
     for i in batch:
@@ -62,26 +62,28 @@ def update_params(buffer, batch_size, main_network: DQN, target_network: DQN, op
         done_tensor[ctr] = i["done"]
         ctr += 1
 
-    q_out = main_network(state_tensor).gather(1, action_tensor).squeeze()   
+    q_out = main_network(state_tensor).gather(1, action_tensor.unsqueeze(1)).squeeze()   
 
     with torch.no_grad():
         q_target = torch.max(target_network(next_state_tensor),1)[0]
-        expected_reward = reward_tensor + q_target
+        expected_reward = reward_tensor + gamma * q_target * (1 - done_tensor)
+
+    loss = nn.MSELoss()(q_out, expected_reward)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
 
 
-
-
-if __name__ == "__main__":
-
-    normalization_tensor = torch.tensor([[[255 for _ in range(128)] for _ in range(128)] for _ in range(3)], dtype=torch.double)
+def train():
 
     N_ACTIONS = 8
 
-    num_episodes = 10 #10000
+    num_episodes = 10000
     update_frequency = 10
     target_update_frequency = 200
-    epsilon = 1.0
+    epsilon = 1
     epsilon_decay = 0.995
     epsilon_min = 0.05
     batch_size=64
@@ -89,8 +91,8 @@ if __name__ == "__main__":
     buffer_size=100000
     learning_rate = 0.001
 
-    main_network = DQN(N_ACTIONS)
-    target_network = DQN(N_ACTIONS)
+    main_network = DQN(N_ACTIONS).float()
+    target_network = DQN(N_ACTIONS).float()
     target_network.load_state_dict(main_network.state_dict())
     target_network.eval()
     main_network.train()
@@ -101,7 +103,8 @@ if __name__ == "__main__":
 
     env = Hw2Env(n_actions=N_ACTIONS, render_mode="gui")
     for episode in range(num_episodes):
-        state = env.reset()
+        env.reset()
+        state = env.state()
         done = False
         cumulative_reward = 0.0
         episode_steps = 0
@@ -109,6 +112,7 @@ if __name__ == "__main__":
             action = get_action(epsilon, main_network, state)
             
             next_state, reward, is_terminal, is_truncated = env.step(action)
+            next_state = next_state 
             done = is_terminal or is_truncated
             buffer.append({"state": state, "action": action,"reward": reward, "next_state": next_state, "done": 0 if done else 1})
 
@@ -118,8 +122,10 @@ if __name__ == "__main__":
             if episode_steps % target_update_frequency == 0: 
                 target_network.load_state_dict(main_network.state_dict())
             if episode_steps % update_frequency == 0:
-                update_params(buffer, batch_size, main_network, target_network, optimizer)
+                update_params(buffer, batch_size, main_network, target_network, optimizer, gamma)
 
         epsilon = max(epsilon_min, epsilon_decay * epsilon)        
 
         print(f"Episode={episode}, reward={cumulative_reward}, RPS={cumulative_reward/episode_steps}")
+
+        
